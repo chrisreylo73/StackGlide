7e18817400d04a27241c591ef3d5c771fbb4e21a
chrisreylo73 <chrisreylo73@gmail.com>
2023-07-08 13:35:17 -0600

    Rethought about how to approach the overall project and the medium to do so.

---
+++ content.js
@@ -0,0 +1,11 @@
+function removeElements() {
+	document.querySelector('.s-navigation').remove();
+	document.querySelector('.s-topbar--content').remove();
+	document.querySelector('#left-sidebar').remove();
+	document.querySelector('#sidebar').remove();
+	document.querySelector('.ml12.aside-cta.flex--item.print\\:d-none.sm\\:ml0.sm\\:mb12.sm\\:order-first.sm\\:as-end').remove();
+	document.querySelector('#footer').remove();
+ }
+ 
+ // Call the function to hide the sidebar
+ removeElements();
